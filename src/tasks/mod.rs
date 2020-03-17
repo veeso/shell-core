e662506beeeb7469c687f5b11fa72a2d52205d51
ChristianVisintin <christian.visintin1997@gmail.com>
2020-03-17 11:51:27 +0100

    pub crate Task command

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -68,7 +68,7 @@ pub struct TaskError {
 /// Task is the entity which describes a single Task and the relation with the next Task in the pipeline
 #[derive(std::fmt::Debug)]
 pub struct Task {
-    command: Vec<String>,            //Command argv
+    pub(crate) command: Vec<String>, //Command argv
     process: Option<Process>,        //Current process in task
     stdout_redirection: Redirection, //Stdout Redirection type
     stderr_redirection: Redirection, //Stderr Redirection type
